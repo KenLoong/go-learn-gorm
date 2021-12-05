4bdd77513c1c549f50cc7dc65030d840263f2291
warson <lungzeongin@163.com>
2021-12-05 21:41:19 +0800

    (connect):how to connect to mysql

---
+++ util/connect.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Connect(dbName string, username string) error {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "user:%s@tcp(127.0.0.1:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn = fmt.Sprintf(dsn, username, dbName)
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	//GORM 使用 database/sql 维护连接池
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Minute * 30)
+	// 设置空闲连接的存活时间
+	sqlDB.SetConnMaxIdleTime(time.Minute * 30)
+	return err
+}
